0f5d79145e69f6aa8599301ca135d394cc5b43f3
Jossy RASOLOHARIJAONA <jrasoloh@e2r2p13.42.fr>
2017-04-09 11:50:05 +0200

    itoa base

--- rendu/ft_itoa_base/ft_itoa_base.c
+++ rendu/ft_itoa_base/ft_itoa_base.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
-#include <stdio.h>
 
-int				ft_int_base_len(int n, int base)
+int				ft_int_len(int n, int base)
 {
 	int			i;
 
-	i = 0;
-	while (n / base > 0)
+	i = 1;
+	while (n / base != 0)
 	{
 		i++;
 		n = n / base;
@@ -14,47 +13,43 @@ int				ft_int_base_len(int n, int base)
 	return (i);
 }
 
-char			ft_convert(int n)
+char			ft_val(int n)
 {
-	if (n > 10)
+	if (n > 9)
 		return (n + 55);
 	return (n + 48);
 }
 
-char			*ft_itoa(int nbr)
+char			*ft_itoa(int n)
 {
 	char		*res;
 	int			i;
 	int			sign;
-	int			copy;
 	int			len;
-
-	if (nbr == -2147483648)
-		return ("-2147483648\n");
-	copy = nbr;
-	sign = 1;
-	len = 0;
-	if (copy < 0)
+	
+	if (n == -2147483648)
+		return("-2147483648");
+	else
 	{
-		sign = -1;
-		len++;
-		copy = -1 * copy;
-	}
-	len = ft_int_base_len(copy, 10) + len;
-	res = (char *)malloc(sizeof(char) * (len + 1));
-	i = 0;
-	res[len] = '\0';
-	len--;
-	if (sign == -1)
-	{
-		res[i] = '-';
-		i++;
-	}
-	while (i <= len)
-	{
-		res[len] = copy % 10 + 48;
+		i = 0;
+		sign = 0;
+		if (n < 0)
+		{
+			sign = 1;
+			n = -1 * n;
+		}
+		len = ft_int_len(n, 10) + sign;
+		res = (char *)malloc(sizeof(char) * (len + 1));
+		res[len] = '\0';
 		len--;
-		copy = copy / 10;
+		if (sign == 1)
+			res[0] = '-';
+		while (len - sign >= 0)
+		{
+			res[len] = n % 10 + 48;
+			n = n / 10;
+			len--;
+		}
 	}
 	return (res);
 }
@@ -62,16 +57,24 @@ char			*ft_itoa(int nbr)
 char			*ft_itoa_base(int value, int base)
 {
 	char		*res;
-	int			len;
+	int			i;
 
-	len = ft_int_base_len(value, base);
-	res = (char *)malloc(sizeof(char) * (len + 1));
-	res[len + 1] = '\0';
-	while (len >= 0)
+	if (base == 10)
+		return (ft_itoa(value));
+	else
 	{
-		res[len] = ft_convert(value % base);
-		len--;
-		value = value / base;
+		if (value < 0)
+			value = -1 * value;
+		res = (char *)malloc(sizeof(char) * (ft_int_len(value, base) + 1));
+		i = ft_int_len(value, base);
+		res[i] = '\0';
+		i--;
+		while (0 <= i)
+		{
+			res[i] = ft_val(value % base);
+			i--;
+			value = value / base;
+		}
 	}
 	return (res);
 }
